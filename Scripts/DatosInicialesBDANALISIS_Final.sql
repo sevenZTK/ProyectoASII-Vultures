15a6e57bdcb5211fdc653225ea809499c3a4018b
Carlos Sandoval <jawicho@outlook.com>
2024-08-08 15:02:35 -0700

    Correcciones a los scripts

--- Scripts/DatosInicialesBDANALISIS_Final.sql
+++ Scripts/DatosInicialesBDANALISIS_Final.sql
@@ -1,15 +1,14 @@
 -- Insertar un registro en la tabla BANCO
-INSERT INTO BANCO (id_cuenta_due�o, nombre_due�o, saldo) 
+INSERT INTO BANCO (id_cuenta_dueno, nombre_dueno, saldo) 
 VALUES (1234567890, 'Juan Perez', 5000.00);
-INSERT INTO BANCO (id_cuenta_due�o, nombre_due�o, saldo) 
+INSERT INTO BANCO (id_cuenta_dueno, nombre_dueno, saldo) 
 VALUES (2535747894, 'Carlos Sandoval', 35000.00);
-INSERT INTO BANCO (id_cuenta_due�o, nombre_due�o, saldo) 
-VALUES (9876543210, 'Mar�a Gonz�lez', 12000.00);
-INSERT INTO BANCO (id_cuenta_due�o, nombre_due�o, saldo) 
+INSERT INTO BANCO (id_cuenta_dueno, nombre_dueno, saldo) 
+VALUES (9876543210, 'Maria Gonzalez', 12000.00);
+INSERT INTO BANCO (id_cuenta_dueno, nombre_dueno, saldo) 
 VALUES (8765432198, 'Ana Ramirez', 7500.00);
-INSERT INTO BANCO (id_cuenta_due�o, nombre_due�o, saldo) 
+INSERT INTO BANCO (id_cuenta_dueno, nombre_dueno, saldo) 
 VALUES (4567891230, 'Pedro Martinez', 28000.00);
-
 
 -- Insertar un registro en la tabla TARJETA
 INSERT INTO TARJETA (nombre_tarjeta, numero_tarjeta, cvv, fecha_vencimiento, id_banco)
@@ -23,8 +22,6 @@ VALUES ('MasterCard', '5555444433331111', '456', '09/27', 4);
 INSERT INTO TARJETA (nombre_tarjeta, numero_tarjeta, cvv, fecha_vencimiento, id_banco)
 VALUES ('American Express', '378282246310005', '321', '12/26', 5);
 
-
-
 -- Insertar registros en la tabla TIPO_USUARIO
 INSERT INTO TIPO_USUARIO (nombre) VALUES ('Administrador');
 INSERT INTO TIPO_USUARIO (nombre) VALUES ('Cliente');
@@ -34,126 +31,124 @@ INSERT INTO USUARIO (id_tipo, nombre, apellido, correo, telefono, contrasena)
 VALUES (1, 'Admin', 'Admin', 'admin@gmail.com', '123456789', 'admin2003');
 
 -- Insertar un registro en la tabla IDIOMA
-INSERT INTO IDIOMA (idioma) VALUES ('Español');
+INSERT INTO IDIOMA (idioma) VALUES ('Espanol');
 
 -- Insertar un registro en la tabla PAIS
 INSERT INTO PAIS (nombre_pais) VALUES ('Guatemala');
 
+-- Categoria principal: Tecnologia
+INSERT INTO CATEGORIA_PRODUCTO (id, id_categoria_padre, nombre_categoria) VALUES (1, NULL, 'Tecnologia');
 
--- Categoría principal: Tecnología
-INSERT INTO CATEGORIA_PRODUCTO (id, id_categoria_padre, nombre_categoria) VALUES (1, NULL, 'Tecnología');
-
--- Subcategorías de Tecnología
+-- Subcategorias de Tecnologia
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (1, 'Computadores y Accesorios');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (1, 'Celulares y Accesorios');
 
--- Subcategorías de Computadores 
+-- Subcategorias de Computadores 
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (2, 'Computadoras');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (2, 'Componentes');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (2, 'Accesorios Computadoras');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (2, 'Impresoras');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (2, 'Monitores');
 
--- Subcategorías de Celulares
+-- Subcategorias de Celulares
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (3, 'Celulares');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (3, 'Accesorios Celulares');
 
--- Categoría principal: Moda
+-- Categoria principal: Moda
 INSERT INTO CATEGORIA_PRODUCTO (id, id_categoria_padre, nombre_categoria) VALUES (11, NULL, 'Moda');
 
--- Subcategorías de Moda
+-- Subcategorias de Moda
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (11, 'Hombres');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (11, 'Mujeres');
-INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (11, 'Niños');
+INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (11, 'Ninos');
 
--- Subcategorías de Hombres
-INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (14, 'Ropa Niños');
-INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (14, 'Zapatos Niños');
+-- Subcategorias de Hombres
+INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (14, 'Ropa Ninos');
+INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (14, 'Zapatos Ninos');
 
--- Subcategorías de Mujeres
+-- Subcategorias de Mujeres
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (12, 'Ropa Hombre');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (12, 'Zapatos Hombre');
 
--- Subcategorías de Niños
+-- Subcategorias de Ninos
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (13, 'Ropa Mujer');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (13, 'Zapatos Mujer');
 
--- Categoría principal: Hogar
+-- Categoria principal: Hogar
 INSERT INTO CATEGORIA_PRODUCTO (id, id_categoria_padre, nombre_categoria) VALUES (21, NULL, 'Hogar');
 
--- Subcategorías de Hogar
-INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (21, 'Electrodomésticos');
+-- Subcategorias de Hogar
+INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (21, 'Electrodomesticos');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (21, 'Muebles');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (21, 'Cocina');
 
--- Subcategorías de Muebles
+-- Subcategorias de Muebles
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (23, 'Sala');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (23, 'Comedor');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (23, 'Dormitorio');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (23, 'Oficina');
 
--- Subcategorías de Sala
+-- Subcategorias de Sala
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (25, 'Sillones');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (25, 'Mesas Sala');
-INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (25, 'Estanterías');
+INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (25, 'Estanterias');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (25, 'Muebles Varios Sala');
 
--- Subcategorías de Comedor
+-- Subcategorias de Comedor
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (26, 'Mesas Comedor');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (26, 'Sillas Comedor');
 
--- Subcategorías de Dormitorio
+-- Subcategorias de Dormitorio
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (27, 'Camas');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (27, 'Armarios');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (27, 'Mesas de Noche');
 
--- Subcategorías de Oficina
+-- Subcategorias de Oficina
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (28, 'Escritorios');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (28, 'Sillas Oficina');
 
--- Subcategorías de Cocina
+-- Subcategorias de Cocina
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (24, 'Utensilios');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (24, 'Vajilla');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (24, 'Recipientes');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (24, 'Accesorios de Cocina');
 
--- Subcategorías de Niños
-INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (15, 'Camisas Niños');
-INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (15, 'Pantalones Niños');
-INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (15, 'Shorts Niños');
+-- Subcategorias de Ninos
+INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (15, 'Camisas Ninos');
+INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (15, 'Pantalones Ninos');
+INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (15, 'Shorts Ninos');
 
--- Subcategorías de Zapatos Hombre
-INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (16, 'Zapatos Deportivos Niños');
-INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (16, 'Sneakers Niños');
+-- Subcategorias de Zapatos Hombre
+INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (16, 'Zapatos Deportivos Ninos');
+INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (16, 'Sneakers Ninos');
 
--- Subcategorías de Hombre
+-- Subcategorias de Hombre
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (17, 'Camisas Hombre');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (17, 'Pantalones Hombre');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (17, 'Shorts Hombre');
 
--- Subcategorías de Zapatos Mujer
+-- Subcategorias de Zapatos Mujer
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (18, 'Zapatos Deportivos Hombre');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (18, 'Sneakers Hombre');
 
--- Subcategorías de Ropa Niños
+-- Subcategorias de Ropa Ninos
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (19, 'Camisas Mujer');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (19, 'Pantalones Mujer');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (19, 'Shorts Mujer');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (19, 'Blusas');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (19, 'Vestidos');
 
--- Subcategorías de Zapatos Niños
+-- Subcategorias de Zapatos Ninos
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (20, 'Zapatos Deportivos Mujer');
 INSERT INTO CATEGORIA_PRODUCTO (id_categoria_padre, nombre_categoria) VALUES (20, 'Sneakers Mujer');
 
-
--- Creación de variaciones
+-- Creacion de variaciones
 INSERT INTO VARIACION (nombre) VALUES ('TallaLetra');
 INSERT INTO VARIACION (nombre) VALUES ('TallaNumerica');
 INSERT INTO VARIACION (nombre) VALUES ('Color');
 INSERT INTO VARIACION (nombre) VALUES ('Almacenamiento');
 
--- Opciones de variación para Color
+-- Opciones de variacion para Color
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (3, 'Rojo');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (3, 'Azul');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (3, 'Verde');
@@ -164,18 +159,18 @@ INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (3, 'Amarillo');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (3, 'Naranja');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (3, 'Morado');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (3, 'Rosa');
-INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (3, 'Marrón');
+INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (3, 'Marron');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (3, 'Dorado');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (3, 'Beige');
 
--- Opciones de variación para TallaLetra
+-- Opciones de variacion para TallaLetra
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (1, 'XS');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (1, 'S');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (1, 'M');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (1, 'L');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (1, 'XL');
 
--- Opciones de variación para TallaNumerica
+-- Opciones de variacion para TallaNumerica
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (2, '5');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (2, '6');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (2, '7');
@@ -200,7 +195,7 @@ INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (2, '38');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (2, '40');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (2, '42');
 
--- Opciones de variación para Almacenamiento
+-- Opciones de variacion para Almacenamiento
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (4, '32GB');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (4, '64GB');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (4, '128GB');
@@ -209,10 +204,9 @@ INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (4, '500GB');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (4, '1TB');
 INSERT INTO OPCION_VARIACION (id_variacion, valor) VALUES (4, '2TB');
 
-
--- Métodos de Envío
-INSERT INTO METODO_ENVIO (nombre, precio) VALUES ('Envío estándar', 10);
-INSERT INTO METODO_ENVIO (nombre, precio) VALUES ('Envío Express', 25);
+-- Metodos de Envio
+INSERT INTO METODO_ENVIO (nombre, precio) VALUES ('Envio estandar', 10);
+INSERT INTO METODO_ENVIO (nombre, precio) VALUES ('Envio Express', 25);
 INSERT INTO METODO_ENVIO (nombre, precio) VALUES ('Cargo Expreso', 25);
 
 -- Inserciones para la tabla ESTADO_ORDEN
@@ -225,6 +219,7 @@ INSERT INTO ESTADO_ORDEN (estado) VALUES ('Entregado');
 INSERT INTO ESTADO_PRODUCTO (estado) VALUES ('DISPONIBLE');
 INSERT INTO ESTADO_PRODUCTO (estado) VALUES ('AGOTADO');
 INSERT INTO ESTADO_PRODUCTO (estado) VALUES ('ELIMINADO');
+
 
 
 SELECT * FROM CATEGORIA_PRODUCTO;
