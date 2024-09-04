2e6a2783717c7cadad639910dd903f590edd4903
Carlos Sandoval <jawicho@outlook.com>
2024-09-04 15:07:39 -0700

    Se añade boton para agregar a la wishlist

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -5,9 +5,8 @@ import star_dull_icon from "../Assets/star_dull_icon.png";
 import { ShopContext } from "../../Context/ShopContext";
 import { AuthContext } from "../../App";
 
-
 const ProductDisplay = (props) => {
-  const { addToCart} = useContext(ShopContext); // Obtener el estado de inicio de sesión del contexto
+  const { addToCart } = useContext(ShopContext); // Obtener el estado de inicio de sesión del contexto
   const [variationOptions1, setVariationOptions1] = useState([]);
   const [variationOptions2, setVariationOptions2] = useState([]);
   const [selectedOption1, setSelectedOption1] = useState("");
@@ -17,11 +16,11 @@ const ProductDisplay = (props) => {
   const [price, setPrice] = useState(null); // Estado para almacenar el precio del producto
   const { isLoggedIn, handleLogin, handleLogout } = useContext(AuthContext);
   const userId = localStorage.getItem("userId");
-  const { cartItemCount,  } = useContext(ShopContext);
+  const { cartItemCount } = useContext(ShopContext);
   const [tipos, setTipos] = useState([]);
   const [mainImage, setMainImage] = useState(props.image1);
   const [smallImages, setSmallImages] = useState([props.image2, props.image3]);
-  
+
   const handleImageClick = (image) => {
     // Intercambiar la imagen clickeada con la imagen principal
     setSmallImages((prevImages) => {
@@ -37,11 +36,11 @@ const ProductDisplay = (props) => {
 
   useEffect(() => {
     // Fetch para obtener las opciones del primer option list
-    fetch(`https://proyectoasii-vultures.onrender.com/firstOption/${props.id}`)
+    fetch(`http://localhost:4000/firstOption/${props.id}`)
       .then(response => response.json())
       .then(data => {
         setVariationOptions1(data.variationOptions1 || []);
-        setTipos(data.variationTypes || []) ;
+        setTipos(data.variationTypes || []);
       })
       .catch(error => console.error('Error fetching first options:', error));
   }, [props.id]);
@@ -51,7 +50,7 @@ const ProductDisplay = (props) => {
     // Si se selecciona una opción en el primer option list, realizar el fetch para obtener las opciones del segundo option list
     if (selectedOption1) {
       if (tipos.length > 1) {
-        fetch(`https://proyectoasii-vultures.onrender.com/secondOption/${selectedOption1}/${props.id}`)
+        fetch(`http://localhost:4000/secondOption/${selectedOption1}/${props.id}`)
           .then(response => response.json())
           .then(data => {
             setVariationOptions2(data.variationOptions2 || []);
@@ -60,7 +59,7 @@ const ProductDisplay = (props) => {
       } else {
         // Si no hay opciones para el segundo select, obtener el precio directamente
         const opcion2 = "0";
-        fetch(`https://proyectoasii-vultures.onrender.com/configurationProduct/${selectedOption1}/${opcion2}/${props.id}`)
+        fetch(`http://localhost:4000/configurationProduct/${selectedOption1}/${opcion2}/${props.id}`)
           .then(response => response.json())
           .then(data => {
             setPrice(data.price); // Establecer el precio del producto
@@ -72,23 +71,21 @@ const ProductDisplay = (props) => {
       setVariationOptions2([]);
     }
   }, [selectedOption1, props.id, tipos]);
-  
-  
 
   const handleSelectOption1 = (e) => {
-  const selectedValue = e.target.value;
-  setSelectedOption1(selectedValue);
-  
-  // Restablecer las opciones del segundo select a un array vacío
-  setVariationOptions2([]);
-};
-  
+    const selectedValue = e.target.value;
+    setSelectedOption1(selectedValue);
+
+    // Restablecer las opciones del segundo select a un array vacío
+    setVariationOptions2([]);
+  };
+
   const handleSelectOption2 = (e) => {
     const selectedValue = e.target.value;
     setSelectedOption2(selectedValue);
 
     // Verificar disponibilidad utilizando el nuevo endpoint en el servidor
-    fetch(`https://proyectoasii-vultures.onrender.com/checkAvailability/${props.id}/${selectedValue}/${selectedOption1}`)
+    fetch(`http://localhost:4000/checkAvailability/${props.id}/${selectedValue}/${selectedOption1}`)
       .then(response => response.json())
       .then(data => {
         if (data.available) {
@@ -121,17 +118,17 @@ const ProductDisplay = (props) => {
       // Mostrar un mensaje de alerta antes de redirigir al usuario al login
       window.alert("Debes iniciar sesión para comprar.");
     }
-    
+
     try {
       // Verificar si hay un carrito existente para el usuario actual
       console.log("ID DE USUARIO QUE VA AL BACKEND: ", userId);
-      const cartResponse = await fetch(`https://proyectoasii-vultures.onrender.com/cart/${userId}`);
+      const cartResponse = await fetch(`http://localhost:4000/cart/${userId}`);
       const cartData = await cartResponse.json();
 
       let cartId;
       // Si no hay un carrito existente, crear uno nuevo
       if (!cartData.cartExists) {
-        const newCartResponse = await fetch(`https://proyectoasii-vultures.onrender.com/cart`, {
+        const newCartResponse = await fetch(`http://localhost:4000/cart`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -143,54 +140,138 @@ const ProductDisplay = (props) => {
       } else {
         cartId = cartData.cartId;
       }
-    console.log("TIPOS EN ADD CARRITO:",tipos);
+
+      console.log("TIPOS EN ADD CARRITO:", tipos);
       // Buscar en la tabla CONFIGURACION_PRODUCTO los registros que coincidan con las opciones seleccionadas
       if (tipos.length > 1) {
-        const configurationResponse = await fetch(`https://proyectoasii-vultures.onrender.com/configurationProduct/${selectedOption1}/${selectedOption2}/${props.id}`);
-        const configurationData = await configurationResponse.json();
-        const itemId = configurationData.itemId;
-      console.log(itemId, cartId);
-      
-
-      // Insertar un nuevo registro en la tabla CARRITO_ITEM o actualizar la cantidad si ya existe
-      const cartItemResponse = await fetch(`https://proyectoasii-vultures.onrender.com/cartItem/${cartId}/${itemId}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ quantity: 1 }),
-      });
-      const cartItemData = await cartItemResponse.json();
-      if (cartItemData.success) {
-        console.log("Producto agregado al carrito con éxito.");
-        window.location.reload();
-      } else {
-        console.error("Error al agregar producto al carrito.");
-      }
-      } else {
-        const configurationResponse = await fetch(`https://proyectoasii-vultures.onrender.com/configurationProduct/${selectedOption1}/0/${props.id}`);
+        const configurationResponse = await fetch(`http://localhost:4000/configurationProduct/${selectedOption1}/${selectedOption2}/${props.id}`);
         const configurationData = await configurationResponse.json();
         const itemId = configurationData.itemId;
         console.log(itemId, cartId);
-        
 
-      // Insertar un nuevo registro en la tabla CARRITO_ITEM o actualizar la cantidad si ya existe
-      const cartItemResponse = await fetch(`https://proyectoasii-vultures.onrender.com/cartItem/${cartId}/${itemId}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ quantity: 1 }),
-      });
-      const cartItemData = await cartItemResponse.json();
-      if (cartItemData.success) {
-        console.log("Producto agregado al carrito con éxito.");
-        window.location.reload();
+        // Insertar un nuevo registro en la tabla CARRITO_ITEM o actualizar la cantidad si ya existe
+        const cartItemResponse = await fetch(`http://localhost:4000/cartItem/${cartId}/${itemId}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ quantity: 1 }),
+        });
+        const cartItemData = await cartItemResponse.json();
+        if (cartItemData.success) {
+          console.log("Producto agregado al carrito con éxito.");
+          window.location.reload();
+        } else {
+          console.error("Error al agregar producto al carrito.");
+        }
       } else {
-        console.error("Error al agregar producto al carrito.");
+        const configurationResponse = await fetch(`http://localhost:4000/configurationProduct/${selectedOption1}/0/${props.id}`);
+        const configurationData = await configurationResponse.json();
+        const itemId = configurationData.itemId;
+        console.log(itemId, cartId);
+
+        // Insertar un nuevo registro en la tabla CARRITO_ITEM o actualizar la cantidad si ya existe
+        const cartItemResponse = await fetch(`http://localhost:4000/cartItem/${cartId}/${itemId}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ quantity: 1 }),
+        });
+        const cartItemData = await cartItemResponse.json();
+        if (cartItemData.success) {
+          console.log("Producto agregado al carrito con éxito.");
+          window.location.reload();
+        } else {
+          console.error("Error al agregar producto al carrito.");
+        }
       }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  // Nueva función para manejar "LO DESEO"
+  const handleAddToWishlist = async () => {
+    if (!selectedOption1) {
+      window.alert("Debes seleccionar una opción en el primer tipo de variación.");
+      return; // Detener la ejecución si el primer option list no tiene una opción seleccionada
+    }
+
+    // Si el segundo option list no está vacío y no tiene una opción seleccionada, mostrar una alerta
+    if (variationOptions2.length > 0 && !selectedOption2) {
+      window.alert("Debes seleccionar una opción en el segundo tipo de variación.");
+      return; // Detener la ejecución si el segundo option list no tiene una opción seleccionada
+    }
+
+    if (!isLoggedIn) {
+      window.alert("Debes iniciar sesión para agregar a la Wishlist.");
+      return;
+    }
+
+    try {
+      // Verificar si hay una Wishlist existente para el usuario actual
+      const wishlistResponse = await fetch(`http://localhost:4000/wishlist/${userId}`);
+      const wishlistData = await wishlistResponse.json();
+
+      let wishlistId;
+      // Si no hay una Wishlist existente, crear una nueva
+      if (!wishlistData.wishlistExists) {
+        const newWishlistResponse = await fetch(`http://localhost:4000/wishlist`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ userId }),
+        });
+        const newWishlistData = await newWishlistResponse.json();
+        wishlistId = newWishlistData.wishlistId;
+      } else {
+        wishlistId = wishlistData.wishlistId;
       }
 
+      // Buscar en la tabla CONFIGURACION_PRODUCTO los registros que coincidan con las opciones seleccionadas
+      if (tipos.length > 1) {
+        const configurationResponse = await fetch(`http://localhost:4000/configurationProduct/${selectedOption1}/${selectedOption2}/${props.id}`);
+        const configurationData = await configurationResponse.json();
+        const itemId = configurationData.itemId;
+
+        // Insertar un nuevo registro en la tabla WISHLIST_ITEM o actualizar si ya existe
+        const wishlistItemResponse = await fetch(`http://localhost:4000/wishlistItem/${wishlistId}/${itemId}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ quantity: 1 }),
+        });
+        const wishlistItemData = await wishlistItemResponse.json();
+        if (wishlistItemData.success) {
+          console.log("Producto agregado a la Wishlist con éxito.");
+          window.location.reload();
+        } else {
+          console.error("Error al agregar producto a la Wishlist.");
+        }
+      } else {
+        const configurationResponse = await fetch(`http://localhost:4000/configurationProduct/${selectedOption1}/0/${props.id}`);
+        const configurationData = await configurationResponse.json();
+        const itemId = configurationData.itemId;
+
+        // Insertar un nuevo registro en la tabla WISHLIST_ITEM o actualizar si ya existe
+        const wishlistItemResponse = await fetch(`http://localhost:4000/wishlistItem/${wishlistId}/${itemId}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ quantity: 1 }),
+        });
+        const wishlistItemData = await wishlistItemResponse.json();
+        if (wishlistItemData.success) {
+          console.log("Producto agregado a la Wishlist con éxito.");
+          window.location.reload();
+        } else {
+          console.error("Error al agregar producto a la Wishlist.");
+        }
+      }
     } catch (error) {
       console.error("Error:", error);
     }
@@ -199,35 +280,25 @@ const ProductDisplay = (props) => {
   // Obtener el precio del producto al seleccionar las opciones
   useEffect(() => {
     if (selectedOption1 && selectedOption2) {
-      fetch(`https://proyectoasii-vultures.onrender.com/configurationProduct/${selectedOption1}/${selectedOption2}/${props.id}`)
+      fetch(`http://localhost:4000/configurationProduct/${selectedOption1}/${selectedOption2}/${props.id}`)
         .then(response => response.json())
         .then(data => {
           setPrice(data.price); // Establecer el precio del producto
         })
         .catch(error => console.error('Error fetching price:', error));
     }
-   
   }, [selectedOption1, selectedOption2]);
 
   return (
     <div className="productdisplay">
       <div className="productdisplay-left">
         <div className="productdisplay-img-list">
-        {smallImages.map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              alt="img"
-              onClick={() => handleImageClick(image)}
-            />
+          {smallImages.map((image, index) => (
+            <img key={index} src={image} alt="img" onClick={() => handleImageClick(image)} />
           ))}
         </div>
         <div className="productdisplay-img">
-        <img
-            className="productdisplay-main-img"
-            src={mainImage}
-            alt="img"
-          />
+          <img className="productdisplay-main-img" src={mainImage} alt="img" />
         </div>
       </div>
       <div className="productdisplay-right">
@@ -263,8 +334,8 @@ const ProductDisplay = (props) => {
           {price && <p>Precio: Q{price.toFixed(2)}</p>}
         </div>
         <h2>Categoria: {props.categoryName}</h2>
-        <button onClick={() => { handleAddToCart(); }} disabled={addToCartDisabled}>ADD TO CART</button>
-
+        <button onClick={handleAddToCart} disabled={addToCartDisabled}>AGREGAR AL CARRITO</button>
+        <button onClick={handleAddToWishlist} disabled={addToCartDisabled}>LO DESEO</button>    
       </div>
     </div>
   );
